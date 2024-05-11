15abc9e71f7df4bf7cb2a1568e866ac59f1d5013
Vishmitha Nipun <it22336354@my.sliit.lk>
2024-05-11 18:24:08 +0530

    alll

--- app/src/main/java/com/example/remind_me/page1.kt
+++ app/src/main/java/com/example/remind_me/page1.kt
@@ -1,27 +1,20 @@
 package com.example.remind_me
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class page1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_page1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Handler().postDelayed({
-            val intent = Intent(this@MainActivity, page1::class.java)
-            startActivity(intent)
-        }, 3000)
-
     }
 }
